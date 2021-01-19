16d8d09ef946965e9f4ad8dd6c01e068cff068a4
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-01-19 16:27:16 +0200

    Ng_HW1

---
+++ src/data/cars.ts
@@ -0,0 +1,9 @@
+export const cars = [
+  {model: 'Toyota', producer: 'Toyota Industries', age: 2019, maxSpeed: 200, motor: 5 },
+  {model: 'Opel', producer: 'Opel Industries', age: 2020, maxSpeed: 250, motor: 3 },
+  {model: 'Nisan', producer: 'Nisan Industries', age: 2012, maxSpeed: 300, motor: 2 },
+  {model: 'Mazda', producer: 'Mazda Industries', age: 2005, maxSpeed: 100, motor: 6 },
+  {model: 'Skoda', producer: 'Skoda Industries', age: 2010, maxSpeed: 120, motor: 1 }
+];
+
+
