79bc3ccded43c19fb83abb0251698754ce4bfdc9
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-01-25 22:26:59 +0200

    HW3_Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/users/user/user.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import { ChosenUserComponent } from './components/chosen-user/chosen-user.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    ChosenUserComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([{
+        path: 'link/users',
+        component: UsersComponent, children: [
+          {
+            path: `:id`,
+            component: ChosenUserComponent
+          }
+        ]
+      }
+      ]
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
