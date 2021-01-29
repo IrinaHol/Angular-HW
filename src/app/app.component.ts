c9ff938ab89802670e2adfebdb4da9203e9290b6
Ira Holovatska <72762647+IrinaHol@users.noreply.github.com>
2021-01-29 20:19:54 +0200

    Forms(Practice)5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HomeworkNg';
+  user = {
+    name: 'Ira',
+    age: 123
+  };
+
+  name = new FormControl('', [this.customBadValidator]);
+  age = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  myForm = new FormGroup({name: this.name, age: this.age});
+
+  xxx(myForm: FormGroup): void {
+    console.log(myForm);
+  }
+
+  customBadValidator(data: AbstractControl): any {
+    console.log('text');
+    if (data.value === 'bad') {
+      return {
+        error: true,
+        msg: 'it is bad words'
+      };
+    }
+  }
 }
+
+
+//
+//   onInputAction(xxx: HTMLInputElement): void {
+//     console.log(xxx.value);
+//   }
+//
+//   submit(form: NgForm): void {
+//     console.log(form.value);
+//   }
+// }
